2b9b58973998b9490ff7820519a5f034c67f088f
Abdul Rehman <mehars.6925@gmail.com>
2021-06-05 01:33:37 +0500

    injected authRouter to express app

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const authRouter = require('./routes/auth')
 const app_config = require('./utils/app_config')
 
 const app = express()
 app_config(app)
+
+app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
