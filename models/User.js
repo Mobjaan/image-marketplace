13cc1a9dd51c570bcaf6e6b444148c5b6770bac7
Abdul Rehman <mehars.6925@gmail.com>
2021-06-05 01:31:25 +0500

    updated user model to hash passwords on save

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('../utils/mongoose')
+const Bcrypt = require('bcrypt')
 const Schema = mongoose.Schema 
 
 const userSchema = new Schema({
@@ -6,5 +7,17 @@ const userSchema = new Schema({
     email: String,
     password: String,
 }, { timestamps: true })
+
+userSchema.pre("save", function(next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+    this.password = Bcrypt.hashSync(this.password, parseInt(process.env.BCRYPT_SALT_ROUNDS));
+    next();
+});
+
+userSchema.methods.comparePassword = function(plaintext, callback) {
+    return callback(null, Bcrypt.compareSync(plaintext, this.password));
+};
 
 module.exports = mongoose.model('User', userSchema) 
