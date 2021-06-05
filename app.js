b058d980d1efe7793d43bb83b032438101123c80
Abdul Rehman <mehars.6925@gmail.com>
2021-06-05 19:20:25 +0500

    configured the routers

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const authRouter = require('./routes/auth')
 const app_config = require('./utils/app_config')
+const categoryRouter = require('./routes/category')
+const dashboardRouter = require('./routes/dashboard')
+const { authenticatedMiddleware } = require('./utils/middlewares')
 
 const app = express()
 app_config(app)
 
 app.use('/auth', authRouter)
+app.use('/categories', authenticatedMiddleware, categoryRouter)
+app.use('/dashboard', authenticatedMiddleware, dashboardRouter)
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
+
+
 
 
 
