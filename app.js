d509975e1a0cd3df2b9610991642a5b45f32b4a3
Abdul Rehman <mehars.6925@gmail.com>
2021-06-07 20:24:53 +0500

    added image routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const authRouter = require('./routes/auth')
 const app_config = require('./utils/app_config')
+const imageRouter = require('./routes/image')
 const categoryRouter = require('./routes/category')
 const dashboardRouter = require('./routes/dashboard')
 const { authenticatedMiddleware } = require('./utils/middlewares')
@@ -9,6 +10,7 @@ const app = express()
 app_config(app)
 
 app.use('/auth', authRouter)
+app.use('/images', authenticatedMiddleware, imageRouter)
 app.use('/categories', authenticatedMiddleware, categoryRouter)
 app.use('/dashboard', authenticatedMiddleware, dashboardRouter)
 
